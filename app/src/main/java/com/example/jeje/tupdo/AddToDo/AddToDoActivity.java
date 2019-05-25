40d31f13cfb315f9678ec21494efbd6cdc09ae63
rajeshsiburian <rajeshsiburian@gmail.com>
2019-05-25 10:26:26 +0700

    first commit

---
+++ app/src/main/java/com/example/jeje/tupdo/AddToDo/AddToDoActivity.java
@@ -0,0 +1,34 @@
+package com.example.jeje.tupdo.AddToDo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+
+import com.example.jeje.tupdo.AppDefault.AppDefaultActivity;
+import com.example.jeje.tupdo.R;
+
+public class AddToDoActivity extends AppDefaultActivity {
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int contentViewLayoutRes() {
+        return R.layout.activity_add_to_do;
+    }
+
+    @NonNull
+    @Override
+    protected Fragment createInitialFragment() {
+        return AddToDoFragment.newInstance();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+}
+
